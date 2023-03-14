7522f6ec3f2751d167c87ae50cd94fb1ad20ca6d
Your Name <you@example.com>
2023-03-14 17:23:15 +0900

    wip

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class HomeController
+{
+    // XXX 1クラスに「1つしかメソッドを書くつもりがない」場合、こういった書き方もできる
+    public function __invoke(Request $request, Response $response, $routeArguments)
+    {
+        // 出力
+        $response->getBody()->write("Top Page");
+        return $response;
+    }
+}
+
+
